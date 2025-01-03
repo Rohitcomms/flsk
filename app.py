2abdbc1244e84a06b4b9e1af53fe8006b90377eb
Rohitcomms <rohitchennuri2@gmail.com>
2025-01-03 09:38:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,22 +5,24 @@ from sumy.summarizers.lsa import LsaSummarizer
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return "Server is running!"
+
 @app.route('/summarize', methods=['POST'])
 def summarize_video():
     try:
-      
         data = request.get_json()
         youtube_url = data.get("url")
         if not youtube_url:
             return jsonify({"error": "No YouTube URL provided"}), 400
-
 
         video = YouTube(youtube_url)
         transcript = video.captions.get_by_language_code('en').generate_srt_captions()
  
         parser = PlaintextParser.from_string(transcript, PlaintextParser.URN_MODE)
         summarizer = LsaSummarizer()
-        summary = summarizer(parser.document, 3) 
+        summary = summarizer(parser.document, 3)
 
         summary_text = " ".join(str(sentence) for sentence in summary)
         return jsonify({"summary": summary_text})
